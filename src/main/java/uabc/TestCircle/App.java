ff618c98625e1d9a5dd579948269848caa04623b
RachelleReyes <reyes.rachelle@uabc.edu.mx>
2018-11-23 09:56:51 -0800

    Actualización

---
+++ src/main/java/uabc/TestCircle/App.java
@@ -0,0 +1,13 @@
+package uabc.TestCircle;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
